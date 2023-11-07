3ac54992fc7e63c3f7c4cc4a4da7f78a9fad1aa4
Oscdivin <divineuwandu84@Gmail.com>
2023-11-07 17:56:20 +0100

    Sign-Up

--- RealEstatefe/src/Pages/Signin.jsx
+++ RealEstatefe/src/Pages/Signin.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Signin() {
   return (
-    <div>
-<h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-<form className='flex justify-center items-center flex-col gap-4'>
+    <div className='p-3 max-w-lg mx-auto'>
+<h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+<form className='flex flex-col gap-4'>
   <input type="text" id='username' placeholder='Username' className='border p-3 rounded-lg'/>
   <input type="email" id='email' placeholder='Email' className='border p-3 rounded-lg'/>
   <input type="password" id='password' placeholder='Password' className='border p-3 rounded-lg'/>
+  <button className='bg-slate-700 text-[#fff] p-3 
+  rounded-lg uppercase hover:opacity-95
+  disabled:opacity-80'>Sign Up</button>
 </form>
+<div>
+   <div className='flex gap-2 mt-3'>
+  <p>Have an account?</p>
+<Link to={"/sign-in"}>
+  <span className='text-blue-700'>Sign In</span>
+</Link>
+ </div>
+</div>
     </div>
   )
 }
