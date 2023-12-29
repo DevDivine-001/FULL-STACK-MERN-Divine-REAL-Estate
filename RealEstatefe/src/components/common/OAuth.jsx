0f3121f6a5c87c3c4bd5f441a671b355276429f9
Oscdivin <divineuwandu84@Gmail.com>
2023-12-30 00:22:32 +0100

    OAuth i areadly now

--- RealEstatefe/src/components/common/OAuth.jsx
+++ RealEstatefe/src/components/common/OAuth.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
-import { FcGoogle } from "react-icons/fc";
+import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import { app } from '../../firebase'
+import { FcGoogle } from 'react-icons/fc'
+import { useDispatch } from 'react-redux'
+import { signInSuccess } from '../../Redux/user/userSlice'
+import { useNavigate } from 'react-router-dom'
 
 export default function OAuth() {
+  const navigate = useNavigate()
+  const dispath = useDispatch()
   const handleGoogleClick = async () =>{
     try {
-      
+      const provider = new GoogleAuthProvider()
+      const auth = getAuth(app)
+
+      const result = await signInWithPopup(auth, provider)
+
+      const res = await fetch("/api/auth/google", {
+        method:"POST",
+        headers:{
+          'Content-Type':"application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email:result.user.email,
+          photo:result.user.photoURL})
+      })
+      const data = await res.json()
+      dispath(signInSuccess(data))
+      navigate("/")
+
+      // console.log(result)
+
     } catch (error) {
       console.log('could not sign in with google', error)
     }
   }
   return (
-    <button className='bg-red-700 text-[#ffff] uppercase hover:opacity-95 rounded-lg p-3 flex justify-center  items-center flex-row gap-1' type='button' 
-    onClick={handleGoogleClick}>
-      Contiunie with <FcGoogle size={22}/>
+    <button
+    onClick={handleGoogleClick}
+    type='button'
+    className='bg-red-700 text-[#ffff] uppercase hover:opacity-95 rounded-lg p-3 flex justify-center  items-center flex-row gap-1' 
+    >
+      Continue with <FcGoogle size={22}/>
     </button>
   )
 }
