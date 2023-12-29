539ccd792925298896b2467b3f3701becc1f2534
Oscdivin <divineuwandu84@Gmail.com>
2023-12-30 00:38:02 +0100

    Declaration is good

--- RealEstatefe/src/App.tsx
+++ RealEstatefe/src/App.tsx
@@ -5,6 +5,7 @@ import SignUp from "../src/Pages/SignUp";
 import Profile from "../src/Pages/Profile";
 import About from "../src/Pages/About";
 import Header from "./components/common/Header";
+import PrivateRoute from "../src/components/common/PrivateRoute";
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/About" element={<About />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/Profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
