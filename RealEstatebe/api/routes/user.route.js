406387e1859643d4f522d5c5ac3d415065a29f88
Oscdivin <divineuwandu84@Gmail.com>
2024-01-09 22:05:42 +0100

    delete file is ready

--- RealEstatebe/api/routes/user.route.js
+++ RealEstatebe/api/routes/user.route.js
@@ -14,6 +14,6 @@ const router = express.Router()
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
-// router.delete("/delete/:id", verifyToken, deleteUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router ;
