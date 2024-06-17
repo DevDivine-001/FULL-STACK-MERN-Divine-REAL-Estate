57915c9ed5e2bdbe2fff9b1b402579817329d720
Oscdivin <divineuwandu84@Gmail.com>
2024-06-17 22:14:27 +0100

    LogOut api is Good and SignOut api

--- RealEstate-WebApp-BE/api/controllers/listing.controller.js
+++ RealEstate-WebApp-BE/api/controllers/listing.controller.js
@@ -1,6 +1,9 @@
+import Listing from "../model/listing.model.js";
+
 export const createListing = async (req,res,next) =>{
     try {
-        
+        const listing = await Listing.create(req.body);
+        return res.status(201).json(listing)
     } catch (error) {
         next(error)
     }
---
+++ RealEstate-WebApp-BE/api/model/listing.model.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true
+
+        },
+        description: {
+            type: String,
+            require: true,
+
+        },
+        address: {
+            type: String,
+            require: true,
+
+        },
+        regularPrice: {
+            type: Number,
+            require: true,
+
+        },
+        discountPrice: {
+            type: Number,
+            require: true,
+
+        },
+        bathrooms: {
+            type: Number,
+            require: true,
+
+        },
+        furnished: {
+            type: Boolean,
+            require: true,
+
+        },
+        parKing: {
+            type: Boolean,
+            require: true,
+
+        },
+        type: {
+            type: String,
+            require: true,
+
+        },
+        offer: {
+            type: Boolean,
+            require: true,
+
+        },
+        imageUrls: {
+            type: Array,
+            require: true,
+
+        },
+        userRef: {
+            type: String,
+            require: true,
+
+        },
+
+    }, { timestamps: true }
+)
+
+const Listing = mongoose.model('Listing', listingSchema)
+
+
+export default Listing
